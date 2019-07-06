17e33c91c73234858b2ba08c3c396865ef87accd
Christopher Bartling <chris.bartling@gmail.com>
2019-07-05 23:42:35 -0500

    Fix for class vs. className.

--- nfl-draftnik-react/src/App.js
+++ nfl-draftnik-react/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       </div>
       <div className="mt-4 md:mt-0 md:ml-6">
         <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
-        <a href="#/fubar" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
+        <a href="#/fubar" className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
           Finding customers for your new business
           </a>
         <p className="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
