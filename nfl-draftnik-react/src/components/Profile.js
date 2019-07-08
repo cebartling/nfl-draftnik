355d0a2484c498181497c0e2c671e006b2326f6c
Christopher Bartling <chris.bartling@gmail.com>
2019-07-07 22:29:03 -0500

    Tweaking the profile view.

--- nfl-draftnik-react/src/components/Profile.js
+++ nfl-draftnik-react/src/components/Profile.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { useAuth0 } from "../react-auth0-wrapper";
+import "./Profile.css";
+import {useAuth0} from "../react-auth0-wrapper";
 
 const Profile = () => {
-    const { loading, user } = useAuth0();
+    const {loading, user} = useAuth0();
 
     if (loading || !user) {
         return "Loading...";
@@ -10,7 +11,9 @@ const Profile = () => {
 
     return (
         <>
-            <img src={user.picture} alt="Profile" />
+            <div className="image-cropper">
+                <img src={user.picture} alt="Profile" className="profile-pic"/>
+            </div>
 
             <h2>{user.name}</h2>
             <p>{user.email}</p>
